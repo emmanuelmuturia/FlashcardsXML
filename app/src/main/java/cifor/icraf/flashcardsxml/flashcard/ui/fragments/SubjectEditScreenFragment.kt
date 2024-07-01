44f674a529e4334826f2c4949d22c5f872d0f7d0
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-07-01 12:05:28 +0300

    Set up SubjectEditScreen...

--- app/src/main/java/cifor/icraf/flashcardsxml/flashcard/ui/fragments/SubjectEditScreenFragment.kt
+++ app/src/main/java/cifor/icraf/flashcardsxml/flashcard/ui/fragments/SubjectEditScreenFragment.kt
@@ -6,55 +6,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import cifor.icraf.flashcardsxml.R
+import cifor.icraf.flashcardsxml.flashcard.domain.entity.SubjectEntity
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textfield.TextInputEditText
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SubjectEditScreenFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SubjectEditScreenFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_subject_edit_screen, container, false)
+
+        val rootView = inflater.inflate(
+            R.layout.fragment_subject_edit_screen,
+            container,
+            false
+        )
+
+        val toolBar = rootView.findViewById<MaterialToolbar>(
+            R.id.topAppBar
+        )
+        toolBar.setNavigationOnClickListener { requireActivity().onBackPressedDispatcher.onBackPressed() }
+        toolBar.setOnMenuItemClickListener { item ->
+            when(item.itemId) {
+                R.id.btnDone -> {
+                    val subjectName = rootView.findViewById<TextInputEditText>(R.id.editTextSubjectName).text.toString()
+                    if (subjectName.isEmpty()) {
+
+                    } else {
+                        val parentActivity = requireActivity() as SubjectEditListener
+                        parentActivity.onCompleteClick(subjectEntity = SubjectEntity(subjectName = subjectName))
+                    }
+                    true
+                }
+                else -> false
+            }
+        }
+
+        return rootView
+
+    }
+
+
+    interface SubjectEditListener {
+        fun onCompleteClick(subjectEntity: SubjectEntity)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SubjectEditScreenFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SubjectEditScreenFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = SubjectEditScreenFragment()
     }
 }
