f662d0ab64f8b5ba92ba09a790c2f88e46d30cb9
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-07-01 12:53:52 +0300

    Set up FlashcardsScreen...

--- app/src/main/java/cifor/icraf/flashcardsxml/flashcard/ui/fragments/FlashcardsScreenFragment.kt
+++ app/src/main/java/cifor/icraf/flashcardsxml/flashcard/ui/fragments/FlashcardsScreenFragment.kt
@@ -1,60 +1,92 @@
 package cifor.icraf.flashcardsxml.flashcard.ui.fragments
 
+import FlashcardsPagerAdapter
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.viewpager2.widget.ViewPager2
 import cifor.icraf.flashcardsxml.R
+import cifor.icraf.flashcardsxml.flashcard.domain.entity.FlashcardEntity
+import cifor.icraf.flashcardsxml.flashcard.ui.viewmodel.FlashcardsXMLViewModel
+import com.google.android.material.appbar.MaterialToolbar
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FlashcardsScreenFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FlashcardsScreenFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var viewPager: ViewPager2
+    private lateinit var flashcardsPagerAdapter: FlashcardsPagerAdapter
+    private val flashcardsXMLViewModel: FlashcardsXMLViewModel by viewModels<FlashcardsXMLViewModel>()
+    private var flashcards = emptyList<FlashcardEntity>()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_flashcards_screen, container, false)
+
+        val rootView = inflater.inflate(
+            R.layout.fragment_flashcards_screen,
+            container,
+            false
+        )
+
+        viewPager = rootView.findViewById(R.id.viewPager)
+        val toolBar = rootView.findViewById<MaterialToolbar>(R.id.flashcardsScreenTopAppBar)
+
+        toolBar.setNavigationOnClickListener { requireActivity().onBackPressed() }
+        toolBar.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.btnAddFlashcard -> {
+                    val parentActivity = requireActivity() as? FlashcardsListener
+                    parentActivity?.onAddNewFlashcardClicked()
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(state = Lifecycle.State.STARTED) {
+                flashcardsXMLViewModel.subjectUIState.collect { uiState ->
+                    uiState.subjects.map { subjects ->
+                        flashcards = subjects.flashcards
+                    }
+                }
+            }
+        }
+
+        viewPager = view?.findViewById(R.id.viewPager)!!
+        flashcardsPagerAdapter = FlashcardsPagerAdapter(
+            flashcards = flashcards
+        )
+        viewPager.adapter = flashcardsPagerAdapter
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                // Reset card to front side when page changes
+                (viewPager.getChildAt(0) as? FlashcardsPagerAdapter.FlashcardViewHolder)?.flipCard()
+            }
+        })
+
+        return rootView
+
+    }
+
+    interface FlashcardsListener {
+        fun onAddNewFlashcardClicked()
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FlashcardsScreenFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FlashcardsScreenFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = FlashcardsScreenFragment()
     }
+
 }
