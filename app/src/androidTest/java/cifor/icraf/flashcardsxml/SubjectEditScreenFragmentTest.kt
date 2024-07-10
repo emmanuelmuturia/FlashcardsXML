06ac02f5e8cb0d79c9290f7cdfbfe60ad45a0661
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-07-10 12:24:10 +0300

    Set up and run The SubjectEditScreenFragment Tests...

--- app/src/androidTest/java/cifor/icraf/flashcardsxml/SubjectEditScreenFragmentTest.kt
+++ app/src/androidTest/java/cifor/icraf/flashcardsxml/SubjectEditScreenFragmentTest.kt
@@ -1,2 +1,49 @@
 package cifor.icraf.flashcardsxml
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import cifor.icraf.flashcardsxml.main.activity.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(value = AndroidJUnit4::class)
+@LargeTest
+class SubjectEditScreenFragmentTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun toolBarExists() {
+        onView(withId(R.id.addSubjectButton)).perform(click())
+        onView(withId(R.id.subjectEditScreenToolbar)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun toolBarHasClickableButtons() {
+        onView(withId(R.id.addSubjectButton)).perform(click())
+
+        onView(withId(R.id.subjectEditScreenToolbar)).check(matches(hasDescendant((withId(R.id.subjectEditScreenBackButton)))))
+            .perform(click())
+
+        onView(withId(R.id.subjectEditScreenToolbar)).check(matches(hasDescendant(withId(R.id.subjectEditScreenDoneButton))))
+            .perform(
+                click()
+            )
+    }
+
+    @Test
+    fun textViewExists() {
+        onView(withId(R.id.addSubjectButton)).perform(click())
+        onView(withId(R.id.editTextSubjectName)).check(matches(isDisplayed()))
+    }
+
+}
